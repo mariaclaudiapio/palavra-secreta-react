4798d3b569a6c5599ead8105066a489f4e941e52
mariaclaudiapio <mariaclaudiapio@gmail.com>
2022-12-23 09:45:56 -0300

    first commit

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ['motor', 'porta', 'capô', 'pneu', 'antena'],
+    fruta: ['banana', 'maçã', 'pêra', 'mamão', 'laranja'],
+    corpo: ['braço', 'perna', 'cérebro', 'pescoço', 'olhos'],
+    computador: ['linguagem', 'framework', 'JavaScript', 'react', 'angular'],
+    alimento: ['arroz', 'feijão', 'carne', 'leite', 'ovos'],
+    signos: ['aries', 'touro', 'gêmeos', 'câncer', 'leão', 'virgem', 'libra', 'escorpião', 'sagitário', 'aquário', 'capricórnio', 'peixes']
+}
